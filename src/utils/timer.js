b4bfb5d6434737952acfb0d8143d314d8849ad9f
Bernard Arhia <bernardarhia@gmail.com>
2020-12-06 14:00:06 +0000

    added task timer

---
+++ src/utils/timer.js
@@ -0,0 +1,21 @@
+
+function timer(hoursNum, minutesNum, secondsNum) {
+    secondsNum--;
+    if (secondsNum == 0) {
+        if (minutesNum == 0) {
+            minutesNum = 0;
+        }
+        else {
+            secondsNum = 59;
+            minutesNum--;
+        }
+    } if (minutesNum == 0 && secondsNum == 0) {
+        if (hoursNum !== 0) {
+            hoursNum--;
+            minutesNum = 59;
+            secondsNum = 59;
+        } else {
+            return;
+        }
+    }
+}
